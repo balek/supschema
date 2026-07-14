d609c117650f78d72ddc4ca953f1795a17985ece
Alexey Balekhov <alexeybalekhov@gmail.com>
2026-07-14 13:19:31 +0200

    Fix typo

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   ssr: {
     resolve: {
-      conditions: ['subschema-dev', 'import', 'default'],
+      conditions: ['supschema-dev', 'import', 'default'],
     },
   },
   test: {
