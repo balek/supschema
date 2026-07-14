61c315270d02a9ee46a34abdc40553c40016a2bc
Alexey Balekhov <alexeybalekhov@gmail.com>
2026-07-14 10:24:59 +0200

    Fix imports in tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  ssr: {
+    resolve: {
+      conditions: ['subschema-dev', 'import', 'default'],
+    },
+  },
   test: {
     include: ['**/*.{test,spec}.{ts,tsx}'],
     environment: 'node',
